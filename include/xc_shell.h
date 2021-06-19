2050e35db7519d62a94f3a6a3351e66a5b20e56c
superxcgm <superxcgm@gmail.com>
2021-06-19 20:22:18 +0800

    Split user input line to command and args

--- include/xc_shell.h
+++ include/xc_shell.h
@@ -2,12 +2,20 @@
 #define INCLUDE_XC_SHELL_H_
 
 #include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
 
 class XcShell {
  public:
   void init();
   void process(std::istream &is, std::ostream &os);
   int exit();
+
+ private:
+  // todo: remove basic type below using typedef or something else
+  static std::tuple<std::string , std::vector<std::string>>
+  parseUserInput(const std::string& str);
 };
 
 #endif  // INCLUDE_XC_SHELL_H_
