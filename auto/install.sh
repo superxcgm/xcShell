d254d7c6983e814b92c9801407f9dac7715f43f3
superxcgm <superxcgm@gmail.com>
2021-10-05 17:03:12 +0800

    Refactor auto command

--- auto/install.sh
+++ auto/install.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+./auto/build.sh
+
 sudo cp build/xcShell /usr/local/bin
 
-if [ ! -f "/etc/xcshrc" ]
-then
-echo "Global config do not exist, auto create from default config."
-sudo cp auto/global_config_default.txt /etc/xcshrc
+if [ ! -f "/etc/xcshrc" ] ;then
+  echo "Global config do not exist, auto create from default config."
+  sudo cp auto/global_config_default.txt /etc/xcshrc
 fi
--- auto/run.sh
+++ auto/run.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-mkdir -p build
-cd build
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  conan install .. -s compiler.libcxx=libstdc++11
-elif [[ "$OSTYPE" == "darwin" ]]; then
-  conan install ..
-else
-  conan install ..
-fi
-cmake ..
-cmake --build .
+./auto/build.sh
 
-./xcShell
+./build/xcShell
