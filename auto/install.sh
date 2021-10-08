ba452a7c905d6c2beba0c5ed13be1d20165cffd8
superxcgm <superxcgm@gmail.com>
2021-10-08 21:54:14 +0800

    Move global_config_default.txt to auto/data

--- auto/install.sh
+++ auto/install.sh
@@ -6,7 +6,7 @@ set -e
 
 sudo cp build/xcShell /usr/local/bin
 
-if [ ! -f "/etc/xcshrc" ] ;then
+if [ ! -f "/etc/xcshrc" ]; then
   echo "Global config do not exist, auto create from default config."
-  sudo cp auto/global_config_default.txt /etc/xcshrc
+  sudo cp auto/data/global_config_default.txt /etc/xcshrc
 fi
