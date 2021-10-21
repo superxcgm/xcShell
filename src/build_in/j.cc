63c55556cbe5da867beba1169920fd19e078aedd
lq1149622825 <lq13892023709@163.com>
2021-10-21 19:26:23 +0800

    fix : fix the cd ~/xx/xx case

--- src/build_in/j.cc
+++ src/build_in/j.cc
@@ -17,21 +17,19 @@ int J::Execute(const std::vector<std::string> &args, std::ostream &os,
   }
 
   std::string path = args.empty() ? "~" : args[0];
-  std::vector<std::string> args_list = utils::Split(path, "/");
-  for (const auto &item : args_list) {
-    path = item;
-    if (item == "~") {
-      path = utils::GetHomeDir();
-    } else if (item == "-") {
-      path = pre;
-      // cover when pre is empty
-      if (path.empty()) {
-        path = ".";
-      }
+  if (path.find('~') != -1) {
+    path = path.replace(path.find('~'), 1, utils::GetHomeDir());
+  } else if (path == "-") {
+    path = pre;
+    // cover when pre is empty
+    if (path.empty()) {
+      path = ".";
     }
-    pre = utils::GetCurrentWorkingDirectory(os_err);
-    ErrorHandling::ErrorDispatchHandler(chdir(path.c_str()),
-                                        ErrorHandling::ErrorType::NORMAL_ERROR);
+  }
+
+  pre = utils::GetCurrentWorkingDirectory(os_err);
+  ErrorHandling::ErrorDispatchHandler(chdir(path.c_str()),
+                                      ErrorHandling::ErrorType::NORMAL_ERROR);
   }
   return 0;
 }
