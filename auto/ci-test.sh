625f25ce53f64e56c53759b653f9d6b553bfe2e7
superxcgm <superxcgm@gmail.com>
2021-11-12 14:40:59 +0800

    Add coverage

---
+++ auto/ci-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#set -e
+
+cd build
+conan install ..
+cmake .. -DCMAKE_CXX_FLAGS="--coverage"
+make
+ctest --output-on-failure
+cd ..
+./auto/functional_test.sh run
+cd build
+gcovr --root .. . --exclude build --sonarqube > ../test-coverage.xml
